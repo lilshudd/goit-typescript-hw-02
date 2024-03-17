2d7ec80fdc37b4909501afe75974cfa944616b71
lilshudd <max.fedorak10@gmail.com>
2024-03-17 20:11:04 +0200

    next

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -3,13 +3,13 @@ interface Page {
   likes: number;
   accounts: string[];
   status: "open" | "close";
-  details: {
+  details?: {
     createAt: Date;
     updateAt: Date;
   };
 }
 
-const page1 = {
+const page1: Page = {
   title: "The awesome page",
   likes: 100,
   accounts: ["Max", "Anton", "Nikita"],
@@ -20,7 +20,7 @@ const page1 = {
   },
 };
 
-const page2 = {
+const page2: Page = {
   title: "Python or Js",
   likes: 5,
   accounts: ["Alex"],
